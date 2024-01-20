b6b4b1aba0f5f8bd807c27b934bc42ccb7018872
PavelPoroskov <pavel.poroskov.se@gmail.com>
2024-01-20 12:49:06 +0300

    organizing

--- readme.txt
+++ readme.txt
@@ -1,8 +1,6 @@
-JavaScript. complex parts
-  debounce, throttle
-TODO	immutability
-TODO	changing nested structures
-TODO	merge structures: lodash method
-TODO	deep clone: lodash method
+TODO	immutability. nested structures
+  TODO	changing nested structures
+  TODO	merge structures: lodash method
+  TODO	deep clone: lodash method
 
 TODO tests for debounce, throttle
---
+++ src/worker/main.js
@@ -0,0 +1,16 @@
+// TODO worker return Buffer using postMessage()
+//  https://nodejs.org/api/worker_threads.html#considerations-when-transferring-typedarrays-and-buffers
+//  https://nodejs.org/api/worker_threads.html#portpostmessagevalue-transferlist
+//
+//  can we pass stream?
+//    browser can https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Transferable_objects
+
+
+// exit worker
+//  when exit main
+//  exit from main worker.terminate()
+//  worker-self-exit
+//  not-exiting worker? timeout
+
+// TODO using worker pool
+//    https://nodejs.org/api/async_context.html#using-asyncresource-for-a-worker-thread-pool
