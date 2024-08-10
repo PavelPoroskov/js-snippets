f4b4b8da71bf1dd18a67e1d233017146d010cf10
PavelPoroskov <pavel.poroskov.se@gmail.com>
2024-08-10 11:14:15 +0200

    extend Set and Map

---
+++ src/small/data-structure.js
@@ -0,0 +1,25 @@
+export class ExtraSet extends Set {
+  addList(list) {
+    list.forEach((i) => {
+      super.add(i);
+    })
+  }
+}
+
+export class ExtraMap extends Map {
+  sum(key, addValue) {
+    super.set(key, (super.get(key) || 0) + addValue)
+  }
+  update(key, updateObj) {
+    super.set(key, {
+      ...super.get(key),
+      ...updateObj,
+    })
+  }
+  concat(key, inItem) {
+    const item = inItem === undefined ? [] : inItem
+    // item -- single item or array
+    const ar = super.get(key) || []
+    super.set(key, ar.concat(item))
+  }
+}
